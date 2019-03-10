0240eb3d39cdf28403100d83a4d894c5e607ad7a
Tashi Gurung <tashigurung@gmail.com>
2019-03-10 15:28:24 -0400

    Add url patterns, views and templates for those patterns

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from adoption import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^adoption/(\d+)/', views.pet_detail, name='pet_detail'),
 ]
